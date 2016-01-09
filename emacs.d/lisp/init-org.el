b0ec13957df40f7a9d139265d047a324e9bc9d0e
root <root@localhost.localdomain>
2016-01-09 16:43:52 +0800

    new blog arch

--- emacs.d/lisp/init-org.el
+++ emacs.d/lisp/init-org.el
@@ -8,27 +8,18 @@
 
 (setq org-src-fontify-natively t)
 (setq org-startup-folded 'showall)
-(setq org-publish-project-alist
-      '(("org"
- 	 :base-directory "."
-	 :base-extension "org"
-	 :with-tags t
-	 :publishing-function org-html-publish-to-html
-	 :publishing-directory "../tmp")
-	("html"
-	 :base-directory "../tmp"
-	 :base-extension "html"
-	 :auto-sitemap t
-	 :sitemap-filename "index.org"
-	 :sitemap-title "sitemap"
-	 :sitemap-sort-files 'anti-chronologically
-	 :publishing-function org-publish-attachment
-	 :publishing-directory "../blogs")
-	("sitemap"
-	 :base-directory "../blogs"
-	 :base-extension "org"
-	 :publishing-function org-html-publish-to-html
-	 :publishing-directory "../blogs")))
+(setq
+ org-link-abbrev-alist
+ '(("blog" . "http://zzyongx.github.io/blogs/%s.html")))
+
+(setq
+ org-publish-project-alist
+ '(("org"
+    :base-directory "."
+    :base-extension "org"
+    :with-tags t
+    :publishing-function org-html-publish-to-html
+    :publishing-directory "../blogs")))
 (setq org-export-babel-evaluate nil)
 (setq org-confirm-babel-evaluate nil)
 
