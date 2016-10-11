44eb221c222b4a1cb852dab54fedab63f1b77845
zhengzhiyong <zhengzhiyong@sogou-inc.com>
2016-10-11 10:26:46 +0800

    feature:GlobalExceptionHandler add ResourceAccessException

--- java/howto/src/main/java/example/api/GlobalExceptionHandler.java
+++ java/howto/src/main/java/example/api/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package example.api;
 import com.google.common.base.Throwables;
 import org.springframework.beans.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.dao.DataAccessException;
 import example.model.*;
@@ -51,5 +52,12 @@ public class GlobalExceptionHandler {
   @ResponseBody
   public ApiResult typeMismatchException(Exception e) {
     return new ApiResult(Errno.BAD_REQUEST, e.toString());
+  }
+
+  @ExceptionHandler(ResourceAccessException.class)
+  @ResponseBody
+  public ApiResult resourceAccessException(Exception e) {
+    e.printStackTrace();
+    return new ApiResult(Errno.INTERNAL_ERROR, e.getMessage());
   }
 }
