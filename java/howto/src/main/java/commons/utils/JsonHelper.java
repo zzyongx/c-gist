5f1e7fc80b7193f53e0cde65654381d1dd7292dd
zhengzhiyong <zhengzhiyong@sogou-inc.com>
2016-10-13 14:49:28 +0800

    feature: JsonHelper

--- java/howto/src/main/java/commons/utils/JsonHelper.java
+++ java/howto/src/main/java/commons/utils/JsonHelper.java
@@ -1,12 +1,35 @@
 package commons.utils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class JsonHelper {
+  static ObjectMapper mapper = new ObjectMapper();  
+  static SimpleModule module = new SimpleModule("java.time.*");
+  static {
+    module.addSerializer(LocalDate.class, new LocalDateJsonSerializer());
+    module.addDeserializer(LocalDate.class, new LocalDateJsonDeserializer());
+    module.addSerializer(LocalDateTime.class, new LocalDateTimeJsonSerializer());
+    module.addDeserializer(LocalDateTime.class, new LocalDateTimeJsonDeserializer());
+    
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    mapper.setSerializationInclusion(Include.NON_NULL);
+    mapper.registerModule(module);
+  }
+
+  public static ObjectMapper factory() {
+    return mapper;
+  }
+
   public static <T> T readValue(String src, Class<T> valueType) {
     try {
-      return new ObjectMapper().readValue(src, valueType);
+      return factory().readValue(src, valueType);
     } catch (Exception e) {
       throw new RuntimeException(e.toString());
     }
@@ -14,7 +37,7 @@ public class JsonHelper {
 
   public static <T> T readValue(String src, TypeReference<T> valueTypeRef) {
     try {
-      return new ObjectMapper().readValue(src, valueTypeRef);
+      return factory().readValue(src, valueTypeRef);
     } catch (Exception e) {
       throw new RuntimeException(e.toString());
     }
@@ -22,7 +45,7 @@ public class JsonHelper {
 
   public static byte[] writeValueAsBytes(Object value) {
     try {
-      return new ObjectMapper().writeValueAsBytes(value);
+      return factory().writeValueAsBytes(value);
     } catch (Exception e) {
       throw new RuntimeException(e.toString());
     }
@@ -30,7 +53,7 @@ public class JsonHelper {
 
   public static String writeValueAsString(Object value) {
     try {
-      return new ObjectMapper().writeValueAsString(value);
+      return factory().writeValueAsString(value);
     } catch (Exception e) {
       throw new RuntimeException(e.toString());
     }
