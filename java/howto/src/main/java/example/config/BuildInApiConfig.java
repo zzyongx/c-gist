1686908e6aba5bd788228a51b5ad727d2a48370a
zhengzhiyong <zhengzhiyong@sogou-inc.com>
2017-08-17 11:33:40 +0800

    BuildInApiConfig remove unused import

--- java/howto/src/main/java/example/config/BuildInApiConfig.java
+++ java/howto/src/main/java/example/config/BuildInApiConfig.java
@@ -1,15 +1,11 @@
 package example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import commons.jsondoc.*;
 
 @Configuration
 public class BuildInApiConfig {
-  @Autowired Environment env;
-
   @Bean(name = "documentationController")
   public JsonDocController jsonDocController() {
     JsonDocController c = new JsonDocController("1.0", "", ProjectInfo.DOC_PKG);
