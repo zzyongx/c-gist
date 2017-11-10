95b9fb413e2ea40879ec52f2585469b189bb0582
zhengzhiyong <zhengzhiyong@sogou-inc.com>
2017-11-10 11:42:10 +0800

    version 1 finish

--- douban2local/js/background.js
+++ douban2local/js/background.js
@@ -1,7 +1,6 @@
 // Called when the user clicks on the page action.
 chrome.pageAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
   chrome.pageAction.setTitle({title: tab.url, tabId: tab.id});
 });
 
@@ -11,10 +10,6 @@ chrome.tabs.query(
     chrome.pageAction.show(tabs[0].id);
   }
 );
-
-chrome.declarativeContent.onPageChanged.getRules(undefined, function (rules) {
-  console.log(rules);
-});
 
 chrome.runtime.onInstalled.addListener(function() {
   // Replace all rules ...
@@ -32,16 +27,3 @@ chrome.runtime.onInstalled.addListener(function() {
     ]);
   });
 });
-
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    c += request.greeting;
-    chrome.storage.sync.set(c);
-
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-
-    sendResponse({farewell: c});
-  });
--- douban2local/js/content.js
+++ douban2local/js/content.js
@@ -1,63 +1,88 @@
-var max = -1
+var dlUrl = "https://www.douban.com/people/zzyong/statuses";
+var stPrefix = "HISTROY_";
 
-var query = {};
-window.location.search.slice(1).split('&').forEach(function(pair) {
-  var parts = pair.split('=');
-  query[parts[0]] = decodeURIComponent(parts[1]);
-});
+function parseQueryString(qs) {
+  var query = {};
+  qs.split('&').forEach(function(pair) {
+    var parts = pair.split('=');
+    query[parts[0]] = decodeURIComponent(parts[1]);
+  });
 
-var href = window.location.href;
-var nextHref;
-if (query.p === undefined) {
-  nextHref = href + (window.location.search == "" ? "?p=2" : "&p=2");
-  query.p = 2;
-} else {
-  query.p++;
-  nextHref = href.replace(/p=\d+/, "p=" + query.p);
+  return query;
 }
 
-// chrome.runtime.sendMessage({greeting: nextHref}, function(response) {
-//   console.log(response);
-// });
-
-// if first page cleanup
-if (query.p == 2) chrome.storage.local.clear();
-
-var streamItemsDom = window.document.getElementsByClassName("stream-items")[0];
-var innerHTML = streamItemsDom.innerHTML;
-
-var keys = [];
-for (var i = 1; i < max; ++i) {
-  keys.push("history_" + i);
+function createHref(url, p, count, init) {
+  return url + "?p=" + p + "&countDownLatch=" + count + "&initCountDownLatch=" + init;
 }
 
-var historyInnerHTML = {};
-chrome.storage.local.get(keys, function(items) {
-  historyInnerHTML["history_" + (query.p-1)] = innerHTML;
-  Object.keys(items).forEach(function(key) { historyInnerHTML[key] = items[key] });
-
-  if (query.p < max) {
-    chrome.storage.local.set(historyInnerHTML);
-    window.setTimeout(function() {
-      console.log(historyInnerHTML);
-      window.location.href = nextHref;
-    }, 5 * 1000);
-  } else {
-    /*
-    var newInnerHTML = ''
-    keys.forEach(function(key) {
-      if (historyInnerHTML[key]) newInnerHTML += historyInnerHTML[key];
-    });
-
-    streamItemsDom.innerHTML = newInnerHTML;
-    alert('打印页面稍后自动打开，届时选择目标打印机为“另存为PDF”即可');
-    window.setTimeout(function() {
-      window.print();
-    }, 5 * 1000); */
+function createStorageKeys(init) {
+  var keys = [];
+  for (var i = 0; i < init; ++i) {
+    keys.push(stPrefix + i);
   }
-});
+  return keys;
+}
+
+function currentStorageKey(query) {
+  var idx = query.initCountDownLatch - query.countDownLatch - 1;
+  return stPrefix + idx;
+}
+
+function storageStats(historyInnerHTML) {
+  var itemCount = 0;
+  var itemSize = 0;
+  Object.keys(historyInnerHTML).forEach(function(key) {
+    itemCount++;
+    itemSize += historyInnerHTML[key].length;
+  });
+  console.log("ItemSizeLimit: 5242880(5M)");
+  console.log("ItemCount:", itemCount, "; ItemSize: ", itemSize);
+}
+
+function nextHrefOrPrint(query) {
+  var streamItemsDom = window.document.getElementsByClassName("stream-items")[0];
+  var innerHTML = streamItemsDom.innerHTML;
+
+  var historyInnerHTML = {};
+  var storageKeys = createStorageKeys(query.initCountDownLatch);
+
+  chrome.storage.local.get(storageKeys, function(items) {
+    historyInnerHTML[currentStorageKey(query)] = innerHTML;
+    Object.keys(items).forEach(function(key) { historyInnerHTML[key] = items[key] });
+
+    storageStats(historyInnerHTML);
+
+    if (query.countDownLatch > 0) {
+      chrome.storage.local.set(historyInnerHTML);
+      window.setTimeout(function() {
+        window.location.href = createHref(dlUrl, parseInt(query.p)+1, query.countDownLatch-1,
+                                          query.initCountDownLatch);
+      }, 5 * 1000);
+    } else {
+      var newInnerHTML = ''
+      storageKeys.forEach(function(key) {  // order is important
+        if (historyInnerHTML[key]) newInnerHTML += historyInnerHTML[key];
+      });
+
+      // console.log(Object.keys(historyInnerHTML));
+      // console.log(historyInnerHTML);
+
+      streamItemsDom.innerHTML = newInnerHTML;
+      alert('打印页面稍后自动打开，届时选择目标打印机为“另存为PDF”即可');
+
+      window.setTimeout(function() {
+        window.print();
+        window.location.href = dlUrl;
+      }, 5 * 1000);
+    }
+  });
+}
+
+var query = parseQueryString(window.location.search.slice(1));
+console.log(query);
+if (query.countDownLatch !== undefined) nextHrefOrPrint(query);
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    alert(JSON.stringify(request));
+    window.location.href = createHref(dlUrl, request.start, request.count-1, request.count);
   });
