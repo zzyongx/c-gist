5ac6d89faaaa31fcad2c2823cd7f9fc042cf5a26
zhengzhiyong <zhengzhiyong@sogou-inc.com>
2017-12-11 20:19:32 +0800

    MapHelper

--- java/howto/src/main/java/commons/utils/MapHelper.java
+++ java/howto/src/main/java/commons/utils/MapHelper.java
@@ -27,4 +27,18 @@ public class MapHelper {
 
     return map;
   }
+
+  // RestTemplate::postForObject if post map<String, String> content-type is application/x-www-form-urlencoded,
+  // if post map<String, Object> content-type is multipart/form-data
+  @SuppressWarnings("unchecked")
+  public static <KEY, VALUE> MultiValueMap<KEY, VALUE> makeMulti2(KEY k, VALUE v, Object ... varArgs) {
+    MultiValueMap<KEY, VALUE> map = new LinkedMultiValueMap<KEY, VALUE>();
+
+    map.add(k, v);
+    for (int i = 0; i < varArgs.length; i+=2) {
+      map.add((KEY) varArgs[i], (VALUE) varArgs[i+1]);
+    }
+
+    return map;
+  }
 }
